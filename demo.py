e9ede1595db59a483b649d55240835c9bb62ede1
theainerd <beingprofess@gmail.com>
2021-04-12 18:23:48 +0530

    added output files

--- demo.py
+++ demo.py
@@ -3,6 +3,8 @@ from utils_data import *
 from utils_algo import *
 from models import *
 
+import numpy
+import pandas as pd
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -25,7 +27,7 @@ parser = argparse.ArgumentParser(
 	add_help=True)
 
 parser.add_argument('-lr', '--learning_rate', help='optimizer\'s learning rate', default=5e-5, type=float)
-parser.add_argument('-bs', '--batch_size', help='batch_size of ordinary labels.', default=256, type=int)
+parser.add_argument('-bs', '--batch_size', help='batch_size of ordinary labels.', default=32, type=int)
 # parser.add_argument('-me', '--method', help='method type. ga: gradient ascent. nn: non-negative. free: Theorem 1. pc: Ishida2017. forward: Yu2018.', choices=['ga', 'nn', 'free', 'pc', 'forward'], type=str, required=True)
 parser.add_argument('-e', '--epochs', help='number of epochs', type=int, default=10)
 parser.add_argument('-wd', '--weight_decay', help='weight decay', default=1e-4, type=float)
@@ -73,7 +75,7 @@ def train_model(model, chosen_loss_c, optimizer, scheduler,K,ccp,num_epochs,meta
 
                     # backward + optimize only if in training phase
                     if phase == 'train':
-                        if args.method == 'ga':
+                        if meta_method == 'ga':
                             if torch.min(loss_vector).item() < 0:
                                 loss_vector_with_zeros = torch.cat((loss_vector.view(-1,1), torch.zeros(K, requires_grad=True).view(-1,1).to(device)), 1)
                                 min_loss_vector, _ = torch.min(loss_vector_with_zeros, dim=1)
@@ -97,7 +99,11 @@ def train_model(model, chosen_loss_c, optimizer, scheduler,K,ccp,num_epochs,meta
 
             epoch_loss = running_loss / dataset_sizes[phase]
             epoch_acc = running_corrects.double() / dataset_sizes[phase]
-            final_epoch_acc.append(epoch_acc)
+            if phase == 'val':
+                final_element = epoch_acc.cpu().numpy()
+                final_element = final_element.item()
+                final_epoch_acc.append(final_element)
+
 
             print('{} Loss: {:.4f} Acc: {:.4f}'.format(
                 phase, epoch_loss, epoch_acc))
@@ -115,8 +121,9 @@ def train_model(model, chosen_loss_c, optimizer, scheduler,K,ccp,num_epochs,meta
     print('Best val Acc: {:4f}'.format(best_acc))
 
     # load best model weights
-    model.load_state_dict(best_model_wts)
-    return model,epoch_acc
+    # model.load_state_dict(best_model_wts)
+    # final_epoch_acc = final_epoch_acc.to_list()
+    return model,final_epoch_acc
 
 args = parser.parse_args()
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -144,24 +151,26 @@ optimizer_conv = optim.SGD(model_conv.parameters(), lr=0.001, momentum=0.9)
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
 
 model_conv,val_acc_forward = train_model(model_conv, chosen_loss_c, optimizer_conv, exp_lr_scheduler,
-                       K,ccp,num_epochs=2,meta_method='forward')
+                       K,ccp,num_epochs=5,meta_method='forward')
+
+print(val_acc_forward)
 
 model_conv,val_acc_free = train_model(model_conv, chosen_loss_c, optimizer_conv, exp_lr_scheduler,
-                       K,ccp,num_epochs=2,meta_method='free')
+                       K,ccp,num_epochs=5,meta_method='free')
 
 model_conv,val_acc_ga = train_model(model_conv, chosen_loss_c, optimizer_conv, exp_lr_scheduler,
-                       K,ccp,num_epochs=2,meta_method='ga')
+                       K,ccp,num_epochs=5,meta_method='pc')
 
 model_conv,val_acc_nn = train_model(model_conv, chosen_loss_c, optimizer_conv, exp_lr_scheduler,
-                       K,ccp,num_epochs=2,meta_method='nn')
+                       K,ccp,num_epochs=5,meta_method='nn')
 
 # Data
-df=pd.DataFrame({'epoch': range(0,args.epochs), 'y1_values': val_acc_forward, 'y2_values': val_acc_free, 'y3_values': val_acc_ga,'y4_values': val_acc_nn })
+df=pd.DataFrame({'epoch': range(0,5), 'y1_values': val_acc_forward, 'y2_values': val_acc_free, 'y3_values': val_acc_ga,'y4_values': val_acc_nn })
  
 # multiple line plots
 plt.plot( 'epoch', 'y1_values', data=df, marker='', markerfacecolor='blue', markersize=12, color='skyblue', linewidth=4,label = 'forward')
 plt.plot( 'epoch', 'y2_values', data=df, marker='', color='red', linewidth=2,label = 'free')
-plt.plot( 'epoch', 'y3_values', data=df, marker='', color='green', linewidth=2, linestyle='dashed', label="ga")
+plt.plot( 'epoch', 'y3_values', data=df, marker='', color='green', linewidth=2, linestyle='dashed', label="pc")
 plt.plot( 'epoch', 'y4_values', data=df, marker='', color='blue', linewidth=2, linestyle='dashed', label="nn")
 # show legend
 plt.legend()
